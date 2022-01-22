29d186d828bb017dc830cef428c9d1f3a899f317
Lucas P Silva <lucas.p.silva90@hotmail.com>
2022-01-22 13:12:06 -0300

    feature: rejects xml body requests

--- src/modules/User/UseCases/ListRepositories/ListUserRepositoriesController.ts
+++ src/modules/User/UseCases/ListRepositories/ListUserRepositoriesController.ts
@@ -6,6 +6,16 @@ import { ListUserRepositoriesUseCase } from './ListUserRepositoriesUseCase';
 class ListUserRepositoriesController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { user } = request.body;
+
+    const bodyDataType = request.accepts();
+
+    if (bodyDataType.some((body) => body.valueOf() === 'application/xml')) {
+      return response.status(406).json({
+        status: 406,
+        message: 'This API accepts only application/json body requests.',
+      });
+    }
+
     try {
       const listUserRepositoriesUseCase = container.resolve(ListUserRepositoriesUseCase);
       const data = await listUserRepositoriesUseCase.execute(user);
