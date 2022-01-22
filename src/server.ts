aac6e45fcb117a6a7529d967100cfc7cbb68272e
Lucas P Silva <lucas.p.silva90@hotmail.com>
2022-01-22 13:10:35 -0300

    chore: added swagger for api documentation

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import express from 'express';
-// import swaggerUi from 'swagger-ui-express';
+import swaggerUi from 'swagger-ui-express';
 
 // import './database';
 
 import './shared/container';
 
 import { router } from './routes';
-// import swaggerFile from './swagger.json';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ type: '*/json' }));
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
